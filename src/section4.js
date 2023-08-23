e5247580ef1dd1d525b2c863e1d8b117b0b2b04a
uttejterlapu <uterlapu.gitam.in>
2023-08-24 00:11:31 +0530

    added

--- src/section4.js
+++ src/section4.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./section4.css";
+import Arrow from "./assts/arrow.svg";
 import Wave from "./assts/wave-6.svg";
 import Wave2 from "./assts/wave-7.svg";
 function section4() {
@@ -10,6 +11,51 @@ function section4() {
       </div>
       <div className="content-section4">
         <h1>Frequently asked questions</h1>
+        <div className="ques">
+          <div className="ques-block">
+            <div className="ques-content">
+              <div className="flex">
+                <div className="ques-green-cir">
+                  <img className="arrow-faq" src={Arrow} alt="arrow"></img>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur </p>
+            </div>
+          </div>
+
+          <div className="ques-block">
+            <div className="ques-content">
+              <div className="flex">
+                <div className="ques-green-cir">
+                  <img className="arrow-faq" src={Arrow} alt="arrow"></img>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur </p>
+            </div>
+          </div>
+
+          <div className="ques-block">
+            <div className="ques-content">
+              <div className="flex">
+                <div className="ques-green-cir">
+                  <img className="arrow-faq" src={Arrow} alt="arrow"></img>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur </p>
+            </div>
+          </div>
+
+          <div className="ques-block">
+            <div className="ques-content">
+              <div className="flex">
+                <div className="ques-green-cir">
+                  <img className="arrow-faq" src={Arrow} alt="arrow"></img>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur </p>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="img-bottom-sec4">
         <img src={Wave2} alt="wave"></img>
